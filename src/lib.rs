3d286070809ee3b29819019b8cae4cf33a750534
Andreas Maier <andreas.martin.maier@desy.de>
2022-02-11 15:48:35 +0100

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -49,27 +49,22 @@
 mod canon;
 mod sparse_graph;
 
-pub use canon::{
-    ToCanon,
-    ToCanonNautySparse,
-    ToCanonTraces,
-    TracesError,
-};
+pub use canon::{ToCanon, ToCanonNautySparse, ToCanonTraces, TracesError};
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use petgraph::{
         graph::{Graph, IndexType, UnGraph},
+        prelude::EdgeIndex,
         EdgeType,
-        prelude::EdgeIndex
     };
 
     fn add_edge<N, E, Ty: EdgeType, Ix: IndexType>(
         g: &mut Graph<N, E, Ty, Ix>,
         v1: usize,
         v2: usize,
-        wt: E
+        wt: E,
     ) -> EdgeIndex<Ix> {
         use petgraph::visit::NodeIndexable;
         g.add_edge(g.from_index(v1), g.from_index(v2), wt)
@@ -88,7 +83,7 @@ mod tests {
         let n_range = (2..20).step_by(2);
 
         for n in n_range {
-            let mut g1 = UnGraph::<(), ()>::with_capacity(n, 3*n);
+            let mut g1 = UnGraph::<(), ()>::with_capacity(n, 3 * n);
             for _ in 0..n {
                 g1.add_node(());
             }
@@ -98,19 +93,19 @@ mod tests {
                 add_edge(&mut g1, i, i + 1, ());
             }
             // Cycle
-            for i in 0..n-2 {
+            for i in 0..n - 2 {
                 add_edge(&mut g1, i, i + 2, ());
             }
             add_edge(&mut g1, 1, n - 2, ());
             add_edge(&mut g1, 0, n - 1, ());
 
-            let mut g2 = UnGraph::<(), ()>::with_capacity(n, 3*n);
+            let mut g2 = UnGraph::<(), ()>::with_capacity(n, 3 * n);
             for _ in 0..n {
                 g2.add_node(());
             }
 
             for i in 0..n {
-                add_edge(&mut g2, i, (i + 1) % n, ());     /* Rim */
+                add_edge(&mut g2, i, (i + 1) % n, ()); /* Rim */
             }
             for i in 0..(n / 2) {
                 add_edge(&mut g2, i, i + n / 2, ()); /* Diagonals */
@@ -123,12 +118,12 @@ mod tests {
 
             let (_, e1) = cg1.into_nodes_edges();
             let mut e1 = Vec::from_iter(
-                e1.into_iter().map(|e| minmax(e.source(), e.target()))
+                e1.into_iter().map(|e| minmax(e.source(), e.target())),
             );
             e1.sort_unstable();
             let (_, e2) = cg2.into_nodes_edges();
             let mut e2 = Vec::from_iter(
-                e2.into_iter().map(|e| minmax(e.source(), e.target()))
+                e2.into_iter().map(|e| minmax(e.source(), e.target())),
             );
             e2.sort_unstable();
             assert_eq!(e1, e2);
