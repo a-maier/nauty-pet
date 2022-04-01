3b3f1ec24fd5a8f47785cd458babacf098c9dd09
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-01 15:08:13 +0200

    trait to check if graphs are identical

---
+++ src/cmp.rs
@@ -0,0 +1,16 @@
+use petgraph::{
+    EdgeType,
+    graph::IndexType,
+    Graph,
+};
+
+pub trait IsIdentical {
+    fn is_identical(&self, other: &Self) -> bool;
+}
+
+impl<N: Eq, E: Eq, Ty: EdgeType, Ix: IndexType> IsIdentical for Graph<N, E, Ty, Ix> {
+    fn is_identical(&self, other: &Self) -> bool {
+        self.node_weights().eq(other.node_weights())
+            && self.edge_references().eq(other.edge_references())
+    }
+}
