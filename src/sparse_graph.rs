ac3e22b96168c96fa6db99c8135ecb01f30e2457
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-01 11:48:04 +0200

    rewrite conversion between petgraph and nauty/Traces

--- src/sparse_graph.rs
+++ src/sparse_graph.rs
@@ -1,25 +1,24 @@
-use std::collections::HashMap;
 use std::convert::From;
-use std::hash::Hash;
+use std::cmp::{Ord, Ordering};
+use std::fmt::Debug;
 use std::os::raw::c_int;
+use std::marker::PhantomData;
 
-use nauty_Traces_sys::{size_t, SparseGraph};
+use ahash::AHashMap;
+use itertools::izip;
+use nauty_Traces_sys::{size_t, SparseGraph as NautySparse};
 use petgraph::{
-    graph::{DefaultIx, DiGraph, Graph, IndexType, UnGraph},
+    graph::{Graph, IndexType},
     visit::EdgeRef,
     EdgeType,
 };
 
 #[derive(Debug, Default, Clone)]
-pub(crate) struct SparseDiGraph<N> {
-    pub(crate) g: SparseGraph,
-    pub(crate) weights: Weights<N>,
-}
-
-#[derive(Debug, Default, Clone)]
-pub(crate) struct SparseUnGraph<N> {
-    pub(crate) g: SparseGraph,
-    pub(crate) weights: Weights<N>,
+pub(crate) struct SparseGraph<N, E, D> {
+    pub(crate) g: NautySparse,
+    pub(crate) node_weights: Weights<N>,
+    edge_weights: AHashMap<(c_int, c_int), Vec<E>>,
+    dir: PhantomData<D>,
 }
 
 #[derive(Debug, Default, Clone, Hash)]
@@ -29,195 +28,225 @@ pub(crate) struct Weights<N> {
     pub(crate) weights: Vec<N>,
 }
 
-impl<N, E, Ix> From<&DiGraph<N, E, Ix>> for SparseDiGraph<N>
+impl<N, E, Ty, Ix> From<Graph<N, E, Ty, Ix>> for SparseGraph<N, E, Ty>
 where
-    Ix: IndexType,
-    N: Hash + Clone + Eq,
-{
-    fn from(g: &DiGraph<N, E, Ix>) -> Self {
-        let Sg(sg) = g.into();
-        let weights = g.get_weights();
-        SparseDiGraph { g: sg, weights }
-    }
-}
-
-impl<N> From<&SparseDiGraph<N>> for DiGraph<N, (), DefaultIx>
-where
-    N: Clone + Default,
-{
-    fn from(sg: &SparseDiGraph<N>) -> Self {
-        let mut edges = Vec::new();
-        for (v1, (d, begin_e)) in sg.g.d.iter().zip(sg.g.v.iter()).enumerate() {
-            let begin_e = *begin_e as usize;
-            let end_e = begin_e + *d as usize;
-            let endpoints = &sg.g.e[begin_e..end_e];
-            for v2 in endpoints {
-                edges.push((v1 as u32, *v2 as u32, ()))
-            }
-        }
-        let mut g = Graph::<N, ()>::from_edges(edges);
-        clone_weights(&sg.weights, &mut g);
-        g
-    }
-}
-
-impl<N> From<&SparseUnGraph<N>> for UnGraph<N, (), DefaultIx>
-where
-    N: Clone + Default,
-{
-    fn from(sg: &SparseUnGraph<N>) -> Self {
-        let mut edges = Vec::new();
-        for (v1, (d, begin_e)) in sg.g.d.iter().zip(sg.g.v.iter()).enumerate() {
-            let begin_e = *begin_e as usize;
-            let end_e = begin_e + *d as usize;
-            let endpoints = sg.g.e[begin_e..end_e].iter();
-            for v2 in endpoints.filter(|&&v| v as usize >= v1) {
-                edges.push((v1 as u32, *v2 as u32, ()))
-            }
-        }
-        let mut g = UnGraph::<N, ()>::from_edges(edges);
-        clone_weights(&sg.weights, &mut g);
-        g
-    }
-}
-
-impl<N, E, Ix> From<&UnGraph<N, E, Ix>> for SparseUnGraph<N>
-where
-    Ix: IndexType,
-    N: Hash + Clone + Eq,
-{
-    fn from(g: &UnGraph<N, E, Ix>) -> Self {
-        let Sg(sg) = g.into();
-        let weights = g.get_weights();
-        SparseUnGraph { g: sg, weights }
-    }
-}
-
-// newtype wrapper so we can implement From
-struct Sg(SparseGraph);
-
-impl<N, E, Ty, Ix> From<&Graph<N, E, Ty, Ix>> for Sg
-where
-    Ix: IndexType,
     Ty: EdgeType,
-    Graph<N, E, Ty, Ix>: CopyDeg + CopyEdges,
+    Ix: IndexType,
+    N: Ord,
+    N: Debug, E: Debug, Ix: Debug,
 {
-    fn from(g: &Graph<N, E, Ty, Ix>) -> Self {
-        let edge_count = if g.is_directed() {
-            g.edge_count()
+    fn from(g: Graph<N, E, Ty, Ix>) -> Self {
+        use petgraph::visit::NodeIndexable;
+        let is_directed = g.is_directed();
+        let mut edges = Vec::from_iter(
+            g.edge_references().map(
+                |e| (g.to_index(e.source()) as c_int, g.to_index(e.target()) as c_int)
+            )
+        );
+        let (nodes, e) = g.into_nodes_edges();
+        let e_weights = Vec::from_iter(e.into_iter().map(|e| e.weight));
+        let mut node_weights = Vec::from_iter(
+            nodes.into_iter().enumerate().map(
+                |(idx, n)| (n.weight, idx)
+            )
+        );
+        // TODO: use sort_by_key, but that has lifetime problems?
+        node_weights.sort_unstable_by(|i, j| i.0.cmp(&j.0));
+        let mut renumber = vec![0; node_weights.len()];
+        for (new_idx, (_, old_idx)) in node_weights.iter().enumerate() {
+            renumber[*old_idx] = new_idx as c_int;
+        }
+        let node_weights = Vec::from_iter(
+            node_weights.into_iter().map(|(wt, _old_idx)| wt)
+        );
+        for edge in &mut edges {
+            edge.0 = renumber[edge.0 as usize];
+            edge.1 = renumber[edge.1 as usize];
+            if !is_directed && edge.0 > edge.1 {
+                std::mem::swap(&mut edge.0, &mut edge.1);
+            }
+        }
+        let g = sg_from(node_weights.len(), &edges, is_directed);
+        let mut edge_weights: AHashMap<_, Vec<E>> = AHashMap::new();
+        for (edge, wt) in izip!(edges, e_weights) {
+            edge_weights.entry(edge).or_default().push(wt)
+        }
+        let mut ptn = vec![1; node_weights.len()];
+        for (ptn, wts) in izip!(&mut ptn, node_weights.windows(2)) {
+            if wts[1] > wts[0] {
+                *ptn = 0;
+            }
+        }
+        if let Some(last) = ptn.last_mut() {
+            *last = 0;
+        }
+        let lab = (0..node_weights.len() as i32).collect();
+        let node_weights = Weights { weights: node_weights, lab, ptn };
+        Self { g, edge_weights, node_weights, dir: PhantomData }
+    }
+}
+
+fn sg_from(
+    num_nodes: usize,
+    edges: &[(c_int, c_int)],
+    is_directed: bool,
+) -> NautySparse {
+    let mut sg = NautySparse::new(
+        num_nodes,
+        if is_directed { edges.len() } else { 2*edges.len() }
+    );
+    let mut adj = vec![vec![]; num_nodes];
+    for (source, target) in edges {
+        adj[*source as usize].push(*target);
+        if !is_directed {
+            adj[*target as usize].push(*source);
+        }
+    }
+    let mut vpos = 0;
+    for (adj, d, v) in izip!(adj, &mut sg.d, &mut sg.v) {
+        *d = adj.len() as c_int;
+        *v = vpos;
+        let start = vpos as usize;
+        let end = start + *d as usize;
+        sg.e[start..end].copy_from_slice(&adj);
+        vpos += *d as size_t
+    }
+    sg
+}
+
+impl<N, E, Ty, Ix> From<SparseGraph<N, E, Ty>> for Graph<N, E, Ty, Ix>
+where
+    Ty: EdgeType,
+    Ix: IndexType,
+    E: Ord,
+N: Debug, E: Debug, Ix: Debug,
+{
+    fn from(mut g: SparseGraph<N, E, Ty>) -> Self {
+        debug_assert_eq!(g.g.v.len(), g.g.d.len());
+        debug_assert_eq!(g.g.v.len(), g.node_weights.lab.len());
+        debug_assert_eq!(g.g.v.len(), g.node_weights.ptn.len());
+        debug_assert_eq!(g.g.v.len(), g.node_weights.weights.len());
+        let is_directed = Ty::is_directed();
+        let nedges = if is_directed {
+            g.g.e.len()
         } else {
-            2 * g.edge_count()
+            debug_assert_eq!(g.g.e.len() % 2, 0);
+            g.g.e.len() / 2
         };
-        let mut sg = SparseGraph::new(g.node_count(), edge_count);
-        g.copy_degrees(&mut sg);
-        update_edges_addr(&mut sg);
-        g.copy_edges(&mut sg);
-        Sg(sg)
-    }
-}
+        let mut res = Graph::with_capacity(g.g.v.len(), nedges);
 
-trait CopyDeg {
-    fn copy_degrees(&self, sg: &mut SparseGraph);
-}
-
-impl<N, E, Ty: EdgeType, Ix: IndexType> CopyDeg for Graph<N, E, Ty, Ix> {
-    fn copy_degrees(&self, sg: &mut SparseGraph) {
-        use petgraph::visit::NodeIndexable;
-        for v in self.node_indices() {
-            let n = self.to_index(v);
-            sg.d[n] = self.edges(v).count() as c_int;
+        // nodes: relabel according to `lab`
+        let mut node_weights = Vec::from_iter(
+            izip!(g.node_weights.lab.iter().copied(), g.node_weights.weights)
+        );
+        node_weights.sort_unstable_by_key(|e| e.0);
+        for (n, i) in node_weights.iter().map(|(i, _w)| i).enumerate() {
+            debug_assert_eq!(n, *i as usize)
         }
-    }
-}
+        for (_, w) in node_weights {
+            res.add_node(w);
+        }
 
-fn update_edges_addr(sg: &mut SparseGraph) {
-    let mut v_idx = 0;
-    for (n, d) in sg.d.iter().enumerate() {
-        sg.v[n] = v_idx;
-        v_idx += *d as size_t;
-    }
-}
+        // edges
+        let mut edges = Vec::new();
+        for (source, (adj_pos, degree)) in izip!(g.g.v, g.g.d).enumerate() {
+            let source = source as c_int;
+            let start = adj_pos as usize;
+            let end = start + degree as usize;
+            let mut odd_self_loop = false;
+            for target in g.g.e[start..end].iter().copied() {
+                if !is_directed {
+                    match source.cmp(&target) {
+                        Ordering::Greater => continue,
+                        Ordering::Less => { },
+                        Ordering::Equal => {
+                            // ignore exactly half of the self-loops
+                            odd_self_loop = !odd_self_loop;
+                            if odd_self_loop {
+                                continue
+                            };
+                        }
+                    };
+                }
+                let w = g.edge_weights
+                    .get_mut(&(source, target)).unwrap()
+                    .pop().unwrap();
 
-trait CopyEdges {
-    fn copy_edges(&self, sg: &mut SparseGraph);
-}
-
-impl<N, E, Ix: IndexType> CopyEdges for DiGraph<N, E, Ix> {
-    fn copy_edges(&self, sg: &mut SparseGraph) {
-        use petgraph::visit::NodeIndexable;
-        for v in self.node_indices() {
-            let n = self.to_index(v);
-            let e_start = sg.v[n] as usize;
-            let e = &mut sg.e[e_start..];
-            for (target, edge) in e.iter_mut().zip(self.edges(v)) {
-                debug_assert_eq!(edge.source(), v);
-                *target = self.to_index(edge.target()) as c_int;
+                // relabel according to `lab`
+                let mut source = g.node_weights.lab[source as usize];
+                let mut target = g.node_weights.lab[target as usize];
+                if !is_directed && source > target {
+                    std::mem::swap(&mut source, &mut target);
+                }
+                edges.push((source, target, w));
             }
         }
+        edges.sort_unstable();
+        for (source, target, weight) in edges {
+            use petgraph::visit::NodeIndexable;
+            let source = res.from_index(source as usize);
+            let target = res.from_index(target as usize);
+            res.add_edge(source, target, weight);
+        }
+        res
     }
 }
 
-impl<N, E, Ix: IndexType> CopyEdges for UnGraph<N, E, Ix> {
-    fn copy_edges(&self, sg: &mut SparseGraph) {
-        use petgraph::visit::NodeIndexable;
-        for v in self.node_indices() {
-            let n = self.to_index(v);
-            let e_start = sg.v[n] as usize;
-            let e = &mut sg.e[e_start..];
-            for (target, edge) in e.iter_mut().zip(self.edges(v)) {
-                let v2 = if v == edge.source() {
-                    edge.target()
-                } else {
-                    debug_assert_eq!(edge.target(), v);
-                    edge.source()
-                };
-                *target = self.to_index(v2) as c_int;
-            }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use testing::GraphIter;
+    use petgraph::{
+        Directed, Undirected,
+        algo::isomorphism::is_isomorphic,
+        graph::{DiGraph, Graph, IndexType, UnGraph},
+        EdgeType,
+    };
+    use log::debug;
+
+    fn log_init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
+    fn tst_conv<N, E, Ty, Ix>(
+        g: Graph<N, E, Ty, Ix>
+    )
+    where
+        N: Debug + Clone + Ord,
+        E: Debug + Clone + Ord,
+        Ty: Debug + EdgeType,
+        Ix: IndexType
+    {
+        debug!("Initial graph: {g:#?}");
+        let s = SparseGraph::from(g.clone());
+        debug!("Nauty graph: {s:#?}");
+        let gg: Graph<N, E, Ty, Ix> = s.into();
+        debug!("Final graph: {gg:#?}");
+        assert!(is_isomorphic(&g, &gg));
+    }
+
+    #[test]
+    fn simple_conversion() {
+        log_init();
+        tst_conv(UnGraph::<(), ()>::from_edges([(0,1), (2,0)]));
+        tst_conv(UnGraph::<(), i32>::from_edges([(0,1, -1), (2,0, 1)]));
+        tst_conv(DiGraph::<(), ()>::from_edges([(0,1), (1,1), (0,2), (2,0)]));
+        tst_conv(DiGraph::<(), u32>::from_edges([(0,1, 0), (1,1, 0), (0,2, 0), (2,0, 1)]));
+    }
+
+    #[test]
+    fn random_conversion_undirected() {
+        log_init();
+        for g in GraphIter::<Undirected>::default().take(1000) {
+            tst_conv(g);
         }
     }
-}
 
-trait GetWeights<N> {
-    fn get_weights(&self) -> Weights<N>;
-}
-
-impl<N, E, Ty: EdgeType, Ix: IndexType> GetWeights<N> for Graph<N, E, Ty, Ix>
-where
-    N: Hash + Clone + Eq,
-{
-    fn get_weights(&self) -> Weights<N> {
-        use petgraph::visit::NodeIndexable;
-        let mut weight_map: HashMap<_, Vec<_>> = HashMap::new();
-        for v in self.node_indices() {
-            let wt = self.node_weight(v).unwrap();
-            let n = self.to_index(v);
-            weight_map.entry(wt).or_default().push(n as c_int);
-        }
-        let mut lab = Vec::with_capacity(self.node_count());
-        let mut ptn = vec![1; self.node_count()];
-        let mut weights = Vec::with_capacity(weight_map.len());
-        for (weight, mut nodes) in weight_map {
-            weights.push(weight.clone());
-            lab.append(&mut nodes);
-            ptn[lab.len() - 1] = 0;
-        }
-        Weights { lab, ptn, weights }
-    }
-}
-
-fn clone_weights<N, E, Ty, Ix>(wt: &Weights<N>, g: &mut Graph<N, E, Ty, Ix>)
-where
-    N: Clone,
-    Ix: IndexType,
-    Ty: EdgeType,
-{
-    let mut wt_idx = 0;
-    for (&v, &end_mark) in wt.lab.iter().zip(wt.ptn.iter()) {
-        use petgraph::visit::NodeIndexable;
-        let v_idx = g.from_index(v as usize);
-        *g.node_weight_mut(v_idx).unwrap() = wt.weights[wt_idx].clone();
-        if end_mark == 0 {
-            wt_idx += 1
+    #[test]
+    fn random_conversion_directed() {
+        log_init();
+        for g in GraphIter::<Directed>::default().take(700) {
+            tst_conv(g);
         }
     }
 }
---
+++ testing/src/lib.rs
@@ -0,0 +1,66 @@
+use std::marker::PhantomData;
+
+use petgraph::{
+    algo::connected_components,
+    graph::Graph,
+    EdgeType,
+};
+use rand::{
+    distributions::Uniform,
+    prelude::*
+};
+use rand_distr::Normal;
+use rand_xoshiro::Xoshiro256Plus;
+
+pub struct GraphIter<Ty: EdgeType> {
+    rng: Xoshiro256Plus,
+    node_distr: Uniform<usize>,
+    wt_distr: Uniform<u8>,
+    edge_distr: Normal<f64>,
+    edge_type: PhantomData<Ty>,
+}
+
+impl<Ty: EdgeType> Default for GraphIter<Ty> {
+    fn default() -> Self {
+        Self {
+            rng: Xoshiro256Plus::seed_from_u64(0),
+            node_distr: Uniform::from(1..10),
+            wt_distr: Uniform::from(0..3),
+            edge_distr: Normal::new(1.0, 1.0).unwrap(),
+            edge_type: PhantomData
+        }
+    }
+}
+
+impl<Ty: EdgeType> Iterator for GraphIter<Ty> {
+    type Item = Graph<u8, u8, Ty>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut rng = &mut self.rng;
+        let mut g = Graph::default();
+        let nnodes = self.node_distr.sample(&mut rng);
+        for _ in 0..nnodes {
+            g.add_node(self.wt_distr.sample(&mut rng));
+        }
+        for i in 0..nnodes {
+            let start = if Ty::is_directed() { 0 } else { i };
+            for j in start..nnodes {
+                let nedges = (0..)
+                    .map(|_| self.edge_distr.sample(&mut rng))
+                    .find(|&n| n >= 0f64)
+                    .unwrap()
+                    .round() as u64;
+                for _ in 0..nedges {
+                    use petgraph::visit::NodeIndexable;
+                    let source = g.from_index(i);
+                    let target = g.from_index(j);
+                    g.add_edge(source, target, self.wt_distr.sample(&mut rng));
+                }
+            }
+        }
+        if connected_components(&g) > 1 {
+            return self.next()
+        }
+        Some(g)
+    }
+}
