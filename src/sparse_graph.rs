8c07c02e91ae56c41e6d92d4c713fbe9f38ddfca
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-05 11:34:20 +0200

    code to support multiple edges

--- src/sparse_graph.rs
+++ src/sparse_graph.rs
@@ -4,7 +4,7 @@ use std::marker::PhantomData;
 use std::os::raw::c_int;
 
 use ahash::AHashMap;
-use itertools::izip;
+use itertools::{izip, Itertools};
 use nauty_Traces_sys::{size_t, SparseGraph as NautySparse};
 use petgraph::{
     graph::{Graph, IndexType},
@@ -65,21 +65,30 @@ where
                 std::mem::swap(&mut edge.0, &mut edge.1);
             }
         }
-        let g = sg_from(node_weights.len(), &edges, is_directed);
         let mut edge_weights: AHashMap<_, Vec<E>> = AHashMap::new();
         for (edge, wt) in izip!(edges, e_weights) {
             edge_weights.entry(edge).or_default().push(wt)
         }
-        let mut ptn = vec![1; node_weights.len()];
+        let edges = Vec::from_iter(
+            edge_weights.iter().map(
+                |((s, t), w)| (*s, *t, w.len())
+            )
+        );
+        let g = sg_from(node_weights.len(), &edges, is_directed);
+        debug_assert!(g.v.len() >= node_weights.len());
+        let mut ptn = vec![1; g.v.len()];
         for (ptn, wts) in izip!(&mut ptn, node_weights.windows(2)) {
             if wts[1] > wts[0] {
                 *ptn = 0;
             }
         }
+        if !node_weights.is_empty() {
+            ptn[node_weights.len() - 1] = 0;
+        }
         if let Some(last) = ptn.last_mut() {
             *last = 0;
         }
-        let lab = (0..node_weights.len() as i32).collect();
+        let lab = (0..g.v.len() as i32).collect();
         let node_weights = Weights {
             weights: node_weights,
             lab,
@@ -96,23 +105,46 @@ where
 
 fn sg_from(
     num_nodes: usize,
-    edges: &[(c_int, c_int)],
+    edges: &[(c_int, c_int, usize)],
     is_directed: bool,
 ) -> NautySparse {
+    let num_edges = edges.iter().map(
+        |(_s, _t, n)| *n
+    ).sum();
+    let num_aux_nodes = num_edges - edges.len();
     let mut sg = NautySparse::new(
-        num_nodes,
+        num_nodes + num_aux_nodes,
         if is_directed {
-            edges.len()
+            num_edges
         } else {
-            2 * edges.len()
+            2 * num_edges
         },
     );
-    let mut adj = vec![vec![]; num_nodes];
-    for (source, target) in edges {
-        adj[*source as usize].push(*target);
-        if !is_directed {
-            adj[*target as usize].push(*source);
+    let mut adj = vec![vec![]; num_nodes + num_aux_nodes];
+    let mut cur_aux_node = num_nodes;
+    for (source, target, num) in edges {
+        use std::iter::once;
+        if source == target {
+            for _ in 0..*num {
+                adj[*source as usize].push(*target);
+                if !is_directed {
+                    adj[*target as usize].push(*source);
+                }
+            }
+            continue;
         }
+        // introduce auxiliary nodes for multiple edges
+        let aux_nodes = cur_aux_node .. cur_aux_node + num - 1;
+        let chain = once(*source as usize)
+            .chain(aux_nodes)
+            .chain(once(*target as usize));
+        for (source, target) in chain.tuple_windows() {
+            adj[source].push(target as c_int);
+            if !is_directed {
+                adj[target].push(source as c_int);
+            }
+        }
+        cur_aux_node += num - 1;
     }
     let mut vpos = 0;
     for (adj, d, v) in izip!(adj, &mut sg.d, &mut sg.v) {
@@ -134,9 +166,9 @@ where
 {
     fn from(mut g: SparseGraph<N, E, Ty>) -> Self {
         debug_assert_eq!(g.g.v.len(), g.g.d.len());
-        debug_assert_eq!(g.g.v.len(), g.node_weights.lab.len());
         debug_assert_eq!(g.g.v.len(), g.node_weights.ptn.len());
-        debug_assert_eq!(g.g.v.len(), g.node_weights.weights.len());
+        debug_assert_eq!(g.g.v.len(), g.node_weights.lab.len());
+        debug_assert!(g.g.v.len() >= g.node_weights.weights.len());
         let is_directed = Ty::is_directed();
         let nedges = if is_directed {
             g.g.e.len()
@@ -144,7 +176,7 @@ where
             debug_assert_eq!(g.g.e.len() % 2, 0);
             g.g.e.len() / 2
         };
-        let mut res = Graph::with_capacity(g.g.v.len(), nedges);
+        let mut res = Graph::with_capacity(g.node_weights.weights.len(), nedges);
 
         // find relabelling from `lab`
         let mut relabel = vec![0; g.node_weights.lab.len()];
@@ -167,12 +199,13 @@ where
 
         // edges
         let mut edges = Vec::new();
-        for (source, (adj_pos, degree)) in izip!(g.g.v, g.g.d).enumerate() {
+        let node_info = g.g.v.iter().zip(g.g.d.iter()).take(res.node_count());
+        for (source, (&adj_pos, &degree)) in node_info.enumerate() {
             let source = source as c_int;
             let start = adj_pos as usize;
             let end = start + degree as usize;
             let mut odd_self_loop = false;
-            for target in g.g.e[start..end].iter().copied() {
+            for mut target in g.g.e[start..end].iter().copied() {
                 if !is_directed {
                     match source.cmp(&target) {
                         Ordering::Greater => continue,
@@ -186,20 +219,39 @@ where
                         }
                     };
                 }
-                let w = g
-                    .edge_weights
-                    .get_mut(&(source, target))
-                    .unwrap()
-                    .pop()
-                    .unwrap();
-
-                // relabel
-                let mut source = relabel[source as usize];
-                let mut target = relabel[target as usize];
-                if !is_directed && source > target {
-                    std::mem::swap(&mut source, &mut target);
+                let mut num_edges = 1;
+                let mut previous = source;
+                // remove auxiliary nodes
+                while target as usize >= res.node_count() {
+                    num_edges += 1;
+                    let d = g.g.d[target as usize];
+                    debug_assert_eq!(d, if is_directed { 1 } else { 2 });
+                    let start = g.g.v[target as usize] as usize;
+                    let end = start + d as usize;
+                    let next = g.g.e[start..end].iter().copied()
+                        .find(|&t| t != previous).unwrap();
+                    previous = target;
+                    target = next;
                 }
-                edges.push((source, target, w));
+                if !is_directed && source > target {
+                    continue;
+                }
+                let mut new_source = relabel[source as usize];
+                let mut new_target = relabel[target as usize];
+                if !is_directed && new_source > new_target {
+                    std::mem::swap(&mut new_source, &mut new_target);
+                }
+
+                for _ in 0..num_edges {
+                    let w = g
+                        .edge_weights
+                        .get_mut(&(source, target))
+                        .unwrap()
+                        .pop()
+                        .unwrap();
+
+                    edges.push((new_source, new_target, w));
+                }
             }
         }
         edges.sort_unstable();
