5fe09eb7f8199911515cb624d32fd3c6f4604d07
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-24 16:15:55 +0200

    absorb self-loops into vertex weights

--- src/sparse_graph.rs
+++ src/sparse_graph.rs
@@ -8,7 +8,7 @@ use ahash::AHashMap;
 use itertools::izip;
 use nauty_Traces_sys::{size_t, SparseGraph as NautySparse};
 use petgraph::{
-    graph::{Graph, IndexType, Node},
+    graph::{Graph, IndexType},
     visit::EdgeRef,
     EdgeType,
 };
@@ -28,34 +28,37 @@ pub(crate) struct Nodes<N> {
     pub(crate) weights: Vec<N>,
 }
 
-fn relabel_to_contiguous_node_weights<N: Ord, Ix: IndexType>(
-    node_weights: impl IntoIterator<Item = Node<N, Ix>>,
-    edges: &mut [(usize, usize)],
-    is_directed: bool,
-) -> Vec<N> {
-    let mut node_weights = Vec::from_iter(
-        node_weights
-            .into_iter()
-            .enumerate()
-            .map(|(idx, n)| (n.weight, idx)),
-    );
-    // TODO: use sort_by_key, but that has lifetime problems?
-    node_weights.sort_unstable_by(|i, j| i.0.cmp(&j.0));
-    let mut renumber = vec![0; node_weights.len()];
-    for (new_idx, (_, old_idx)) in node_weights.iter().enumerate() {
+fn relabel_to_contiguous_node_weights<N: Ord>(
+    nodes: &mut[N],
+) -> Vec<usize> {
+    let mut new_ord = Vec::from_iter(0..nodes.len());
+    new_ord.sort_unstable_by(|&i, &j| nodes[i].cmp(&nodes[j]));
+    let mut renumber = vec![0; new_ord.len()];
+    for (new_idx, old_idx) in new_ord.iter().enumerate() {
         renumber[*old_idx] = new_idx;
     }
-    for edge in edges {
-        edge.0 = renumber[edge.0];
-        edge.1 = renumber[edge.1];
-        if !is_directed && edge.0 > edge.1 {
-            std::mem::swap(&mut edge.0, &mut edge.1);
-        }
+    apply_perm(nodes, renumber.clone());
+    for n in nodes.windows(2) {
+        debug_assert!(n[0] <= n[1]);
     }
-    node_weights.into_iter().map(|(wt, _old_idx)| wt).collect()
+    renumber
 }
 
-impl<N, E, Ty, Ix> From<Graph<N, E, Ty, Ix>> for SparseGraph<N, E, Ty>
+fn apply_perm<T>(slice: &mut [T], mut new_pos: Vec<usize>) {
+    const CORRECT_POS: usize = usize::MAX;
+    for idx in 0..slice.len() {
+        let mut next_idx = new_pos[idx];
+        if next_idx == CORRECT_POS {
+            continue;
+        }
+        while next_idx != idx {
+            slice.swap(idx, next_idx);
+            next_idx = std::mem::replace(&mut new_pos[next_idx], CORRECT_POS);
+        }
+    }
+}
+
+impl<N, E, Ty, Ix> From<Graph<N, E, Ty, Ix>> for SparseGraph<(N, Vec<E>), E, Ty>
 where
     Ty: EdgeType,
     Ix: IndexType,
@@ -65,27 +68,44 @@ where
     fn from(g: Graph<N, E, Ty, Ix>) -> Self {
         use petgraph::visit::NodeIndexable;
         let is_directed = g.is_directed();
-        let mut edges = Vec::from_iter(
+        let edges = Vec::from_iter(
             g.edge_references()
                 .map(|e| (g.to_index(e.source()), g.to_index(e.target()))),
         );
         let (nodes, e) = g.into_nodes_edges();
-        let e_weights = Vec::from_iter(e.into_iter().map(|e| e.weight));
-
-        let node_weights =
-            relabel_to_contiguous_node_weights(nodes, &mut edges, is_directed);
+        let mut node_weights = Vec::from_iter(
+            nodes.into_iter().map(
+                |n| (n.weight, Vec::new())
+            )
+        );
 
         // edge weights
         // we combine multiple edges into a single one with an
         // effective weight given by the sorted vector of the
         // individual weights
+        // self-loops are removed and their weights instead appended
+        // to the corresponding node weight
         let mut edge_weights: AHashMap<_, Vec<E>> = AHashMap::new();
-        for (edge, wt) in izip!(edges, e_weights) {
-            edge_weights.entry(edge).or_default().push(wt)
+        for (edge, wt) in izip!(edges, e.into_iter().map(|e| e.weight)) {
+            if edge.0 == edge.1 {
+                node_weights[edge.0].1.push(wt);
+            } else {
+                edge_weights.entry(edge).or_default().push(wt);
+            }
         }
         for v in edge_weights.values_mut() {
             v.sort_unstable();
         }
+        let relabel = relabel_to_contiguous_node_weights(&mut node_weights);
+        let edge_weights: AHashMap<_, _> = edge_weights.into_iter().map(
+            |(e, wt)| {
+                let mut e = (relabel[e.0], relabel[e.1]);
+                if !is_directed && e.0 > e.1 {
+                    std::mem::swap(&mut e.0, &mut e.1)
+                }
+                (e, wt)
+            }
+        ).collect();
 
         // the edge weight that appears most often is taken to be the default
         // for all other edge weights we introduce auxiliary vertices
@@ -112,19 +132,12 @@ where
             .collect();
 
         // We introduce one auxiliar vertex for each edge with
-        // non-default weight. To eliminate self-loops, we instead
-        // introduce _two_ additional vertices, even if the original
-        // edge had the default weight
-        let mut num_aux = vec![0; aux_vertex_type.len() + 1];
+        // non-default weight.
+        let mut num_aux = vec![0; aux_vertex_type.len()];
         for ((source, target), w) in &edge_weights {
+            debug_assert_ne!(source, target);
             if let Some(&aux_type) = aux_vertex_type.get(w.as_slice()) {
-                if source == target {
-                    num_aux[aux_type] += 2;
-                } else {
-                    num_aux[aux_type] += 1;
-                }
-            } else if source == target {
-                *num_aux.last_mut().unwrap() += 2;
+                num_aux[aux_type] += 1;
             }
         }
 
@@ -158,6 +171,7 @@ where
 
         for (edge, wt) in &edge_weights {
             let (source, target) = *edge;
+            debug_assert_ne!(source, target);
             if let Some(&aux_vx_type) = aux_vertex_type.get(wt.as_slice()) {
                 // non-default weight, introduce an extra vertex
                 add_edge(
@@ -166,12 +180,6 @@ where
                     aux_vx_idx[aux_vx_type],
                     is_directed,
                 );
-                if source == target {
-                    // self-loop, introduce one more vertex
-                    let start = aux_vx_idx[aux_vx_type];
-                    add_edge(&mut adj, start, start + 1, is_directed);
-                    aux_vx_idx[aux_vx_type] += 1;
-                }
                 add_edge(
                     &mut adj,
                     aux_vx_idx[aux_vx_type],
@@ -179,13 +187,6 @@ where
                     is_directed,
                 );
                 aux_vx_idx[aux_vx_type] += 1;
-            } else if source == target {
-                // self-loop with default weight, add two auxiliary vertices
-                let vx_idx = aux_vx_idx.last_mut().unwrap();
-                add_edge(&mut adj, source, *vx_idx, is_directed);
-                add_edge(&mut adj, *vx_idx, *vx_idx + 1, is_directed);
-                add_edge(&mut adj, *vx_idx + 1, target, is_directed);
-                *vx_idx += 2;
             } else {
                 add_edge(&mut adj, source, target, is_directed);
             }
@@ -230,13 +231,13 @@ where
     }
 }
 
-impl<N, E, Ty, Ix> From<SparseGraph<N, E, Ty>> for Graph<N, E, Ty, Ix>
+impl<N, E, Ty, Ix> From<SparseGraph<(N, Vec<E>), E, Ty>> for Graph<N, E, Ty, Ix>
 where
     Ty: EdgeType,
     Ix: IndexType,
     E: Ord,
 {
-    fn from(mut g: SparseGraph<N, E, Ty>) -> Self {
+    fn from(mut g: SparseGraph<(N, Vec<E>), E, Ty>) -> Self {
         debug_assert_eq!(g.g.v.len(), g.g.d.len());
         debug_assert_eq!(g.g.v.len(), g.nodes.ptn.len());
         debug_assert_eq!(g.g.v.len(), g.nodes.lab.len());
@@ -247,7 +248,7 @@ where
         } else {
             debug_assert_eq!(g.g.e.len() % 2, 0);
             g.g.e.len() / 2
-        };
+        } + g.nodes.weights.iter().map(|(_, loops)| loops.len()).sum::<usize>();
         let mut res = Graph::with_capacity(g.nodes.weights.len(), nedges);
 
         // find relabelling from `lab`
@@ -256,19 +257,23 @@ where
             relabel[old as usize] = new;
         }
 
-        // nodes
+        let mut edges = Vec::new();
+
+        // nodes + self-loops
         let mut node_weights =
             Vec::from_iter(izip!(relabel.iter().copied(), g.nodes.weights));
         node_weights.sort_unstable_by_key(|e| e.0);
         for (n, i) in node_weights.iter().map(|(i, _w)| i).enumerate() {
             debug_assert_eq!(n, *i as usize)
         }
-        for (_, w) in node_weights {
+        for (_, (w, loops)) in node_weights {
+            for w in loops {
+                edges.push((res.node_count(), res.node_count(), w))
+            }
             res.add_node(w);
         }
 
         // edges
-        let mut edges = Vec::new();
         let node_info = g.g.v.iter().zip(g.g.d.iter()).take(res.node_count());
         for (source, (&adj_pos, &degree)) in node_info.enumerate() {
             let source = source as c_int;
@@ -281,19 +286,16 @@ where
                     continue;
                 }
                 // remove auxiliary nodes
-                let mut previous = source;
-                while target as usize >= res.node_count() {
+                if target as usize >= res.node_count() {
                     let d = g.g.d[target as usize];
                     debug_assert_eq!(d, if is_directed { 1 } else { 2 });
                     let start = g.g.v[target as usize] as usize;
                     let end = start + d as usize;
-                    let next = g.g.e[start..end]
+                    target = g.g.e[start..end]
                         .iter()
                         .copied()
-                        .find(|&t| t != previous)
+                        .find(|&t| t != source)
                         .unwrap();
-                    previous = target;
-                    target = next;
                 }
                 if !is_directed && source > target {
                     continue;
@@ -304,15 +306,11 @@ where
                     std::mem::swap(&mut new_source, &mut new_target);
                 }
 
-                let weights =
-                    g.edges.remove(&(source as usize, target as usize));
-                if let Some(weights) = weights {
-                    for w in weights {
-                        edges.push((new_source, new_target, w));
-                    }
-                } else {
-                    // the same edge can only be hit twice in self-loops
-                    debug_assert_eq!(source, target);
+                let weights = g.edges
+                    .remove(&(source as usize, target as usize))
+                    .unwrap();
+                for w in weights {
+                    edges.push((new_source, new_target, w));
                 }
             }
         }
