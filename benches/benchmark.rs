ebdc382d365aa84197c9053bae12d0e40d1a7807
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-25 17:35:56 +0200

    benchmark for dense nauty canonisation

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,5 @@
 use nauty_pet::prelude::*;
-use nauty_pet::canon::{IntoCanonNautySparse, IntoCanonTraces};
+use nauty_pet::canon::{IntoCanonNautyDense, IntoCanonNautySparse, IntoCanonTraces};
 use testing::{GraphIter, randomize_labels};
 
 use criterion::{BatchSize, black_box, criterion_group, criterion_main, Criterion};
@@ -11,12 +11,22 @@ use petgraph::{
 use rand::prelude::*;
 use rand_xoshiro::Xoshiro256Plus;
 
-fn iso_nauty<Ty: EdgeType, Ix: IndexType>(
+fn iso_nauty_sparse<Ty: EdgeType, Ix: IndexType>(
     graphs: impl IntoIterator<Item = (Graph<u8, u8, Ty, Ix>, Graph<u8, u8, Ty, Ix>)>
 ) -> bool {
     graphs.into_iter().all(|(g, h)| {
         g.into_canon_nauty_sparse().is_identical(
             &h.into_canon_nauty_sparse()
+        )
+    })
+}
+
+fn iso_nauty_dense<Ty: EdgeType, Ix: IndexType>(
+    graphs: impl IntoIterator<Item = (Graph<u8, u8, Ty, Ix>, Graph<u8, u8, Ty, Ix>)>
+) -> bool {
+    graphs.into_iter().all(|(g, h)| {
+        g.into_canon_nauty_dense().is_identical(
+            &h.into_canon_nauty_dense()
         )
     })
 }
@@ -40,8 +50,12 @@ fn criterion_benchmark(c: &mut Criterion) {
             .map(|g| (g.clone(), randomize_labels(g, &mut rng))),
     );
     let g = graphs.clone();
-    c.bench_function("undirected nauty", move |b| {
-        b.iter_batched(|| g.clone(), |g| iso_nauty(black_box(g)), BatchSize::SmallInput)
+    c.bench_function("undirected sparse nauty", move |b| {
+        b.iter_batched(|| g.clone(), |g| iso_nauty_sparse(black_box(g)), BatchSize::SmallInput)
+    });
+    let g = graphs.clone();
+    c.bench_function("undirected dense nauty", move |b| {
+        b.iter_batched(|| g.clone(), |g| iso_nauty_dense(black_box(g)), BatchSize::SmallInput)
     });
     c.bench_function("undirected traces", move |b| {
         b.iter_batched(|| graphs.clone(), |g| iso_traces(black_box(g)), BatchSize::SmallInput)
@@ -52,8 +66,12 @@ fn criterion_benchmark(c: &mut Criterion) {
             .take(1000)
             .map(|g| (g.clone(), randomize_labels(g, &mut rng))),
     );
-    c.bench_function("directed nauty", move |b| {
-        b.iter_batched(|| graphs.clone(), |g| iso_nauty(black_box(g)), BatchSize::SmallInput)
+    let g = graphs.clone();
+    c.bench_function("directed sparse nauty", move |b| {
+        b.iter_batched(|| g.clone(), |g| iso_nauty_sparse(black_box(g)), BatchSize::SmallInput)
+    });
+    c.bench_function("directed dense nauty", move |b| {
+        b.iter_batched(|| graphs.clone(), |g| iso_nauty_dense(black_box(g)), BatchSize::SmallInput)
     });
 
 }
